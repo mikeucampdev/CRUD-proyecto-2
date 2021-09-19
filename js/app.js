f430d8394d195800c58b668838c2d8656e570dad
hecmontes.c02@ucamp.utel.edu.mx <hemontemayor.c02@ucamp.utel.edu.mx>
2021-09-19 00:44:15 -0500

    tratando que funcione mi CRUD

--- js/app.js
+++ js/app.js
@@ -1,69 +1,44 @@
 
-
-
-let element, i;
-let data = [
-  { name: "Mauricio", lastname: "Sevilla" },
-  { name: "Jorge", lastname: "Barón" },
-  { name: "Andrés", lastname: "Espinoza" },
-  { name: "Rafael", lastname: "Pérez" }
-];
-
-
+ 
+ var list = [
+   { name: "Mauricio", lastname: "Sevilla" },
+   { name: "Jorge", lastname: "Barón" },
+   { name: "Andrés", lastname: "Espinoza" },
+   { name: "Rafael", lastname: "Pérez" }
+ ];
+ 
+ 
 window.onload = () =>{
-  data.push(window.localStorage.getItem('data'))
-  console.log(data);
-  alert('Bienvenidos a mi primer CRUD.Gracias TOTALES!!!')
+  console.log(list);
 }
-
-//Se guardan los datos en local storage
-function guardarLocalStorage (data){
-  window.localStorage.setItem('data', JSON.stringify(data)
-  );
-}
-
-
-
-
-/*function renderItem() {
-
-  panel.textContent = "";
-  data.forEach(x => {
-    element = document.createElement("option");
-    element.innerText = `${x.name} ${x.lastname}`;
-    panel.append(element);
-  });
-}*/
-
-// Aqui se agregan nuevos datos
-function create(data) {
-  let name = document.querySelector("#name").value;
-  let lastname = document.querySelector("#lname").value;
-  data = [...data, { firstname: name, lastname: lastname }];
-  data.push(data);
-  guardarLocalStorage(data);
-  return data;
-  alert('Dato guardado con éxito');
-  console.log(data);
-  
-}
-
-
-//Aqui se actualizan nuevos datos
-function update() {
-  data[i].name = document.querySelector("#name").value;
-  data[i].lastname = document.querySelector("#lname").value;
-  renderItem();
-}
-
-
-//Aqui se borran los datos
-function deleteItem(NombresFiltrados) {
-  const name = data.filter((data) => {
-    return data.name != NombresFiltrados;
-  })
-}
-data = deleteItem;
-guardarLocalStorage(data);
+ 
 
  
+ function create(data) {
+   var name = document.querySelector("#name").value;
+   var lastname = document.querySelector("#lname").value;
+   data.push(name, lastname);
+   return array;
+   
+
+ }
+
+ function deleteItem(name) {
+  const listNames = name.filter((data) => {
+  return data.name !=  
+
+  console.log(listNames);
+  
+})
+ 
+
+ 
+ function update() {
+   list.name = document.querySelector("#name").value;
+   list.lastname = document.querySelector("#lname").value;
+   renderItem();
+ }
+ 
+ 
+ 
+ }
