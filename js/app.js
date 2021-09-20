4973eda2e07cb203464f7c138c57df5d6f9e703c
hecmontes.c02@ucamp.utel.edu.mx <hemontemayor.c02@ucamp.utel.edu.mx>
2021-09-19 22:39:06 -0500

    Boton de agregar funcionando

--- js/app.js
+++ js/app.js
@@ -1,14 +1,24 @@
 
- //toda tarea debe tener un id
- var invent = [
-   { id:1, name: 'calabaza'},
-   { id:2,name: 'durazno'},
-   { id:3, name: 'zanahoria'},
-   { id:4, name: 'piña'},
-  ];
+
+//TODO Selectores del DOM
+
+const inputName   = document.getElementById('name')
+const buttonCreate = document.getElementById('newItem')
+const buttonUpdate = document.getElementById('updateItem')
+const buttonDelete = document.getElementById('deleteItem')
+
+
+
+
+
+ //TODO toda tarea debe tener un id
+ var invent = [];
  
  
-window.onload = () =>{
+window.onload = () => {
+  const inventFormat = JSON.parse(window.localStorage.getItem('invent'))
+  invent = inventFormat
+  alert('Bienvenidos a mi primer CRUD. Gracias TOTALES!!!')
   console.log(invent);
 }
  
@@ -19,37 +29,48 @@ function saveLocalStorage(invent){
 
 
 
- 
+ // Funcion para crear
  function createItem(item) {
   const itemId = {id:Math.random() ,...item}
-  invent.push(itemId);
-  saveLocalStorage(invent);
+  invent.push(itemId)
+  saveLocalStorage(invent)
+  alert('Producto agregado')
   return invent;
    
 
  }
 
+ //Funcion para borrar
  function deleteItem(name) {
-   //Filter es para filtrar los elementos que hagan match con mi condicional
+   //TODO Filter es para filtrar los elementos que hagan match con mi condicional
    const itemFilter = invent.filter((item) => {
    return item.name != name //-> Esta es mi condicional
    })
-   invent = itemFilter;
+   invent = itemFilter
    saveLocalStorage(invent)
   }
   
  
  
 
- 
+ //Funcion para actualizar
  function updateItem(itemEdit) {
   const itemFilter = invent.filter((item) => {
     return item.id != itemEdit.id//-> Esta es mi condicional
     })
-    itemFilter.push(itemEdit);
+    itemFilter.push(itemEdit)
     saveLocalStorage(invent)
-    
+
    }
+
+   //TODO Metodo para que funcionen los botones
+  buttonCreate.addEventListener('click', () =>{
+  const valueInput = inputName .value
+  const name = {nombre: valueInput}
+  createItem(name)
+  inputName.value = ''
+   
+})
    
 
  
